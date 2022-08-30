f99200c8a4bc406d12936224b06e7bbb8c692521
TrixiEther <trixiether@gmail.com>
2022-08-30 15:59:03 +0300

    Feature | Like-Dislike
    Simple likes/dislikes

--- extensions/dvach/src/com/mishiranu/dashchan/chan/dvach/DvachModelMapper.java
+++ extensions/dvach/src/com/mishiranu/dashchan/chan/dvach/DvachModelMapper.java
@@ -14,6 +14,7 @@ import chan.content.model.Icon;
 import chan.content.model.Post;
 import chan.content.model.Posts;
 import chan.content.model.ThreadSummary;
+import chan.content.model.Vote;
 import chan.text.JsonSerial;
 import chan.text.ParseException;
 import chan.util.StringUtils;
@@ -56,6 +57,7 @@ public class DvachModelMapper {
 		public Boolean subjectsEnabled;
 		public Boolean sageEnabled;
 		public Boolean flagsEnabled;
+		public Boolean likesEnabled;
 
 		@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 		public boolean handle(JsonSerial.Reader reader, String name) throws IOException, ParseException {
@@ -146,6 +148,10 @@ public class DvachModelMapper {
 					flagsEnabled = reader.nextBoolean();
 					return true;
 				}
+				case "enable_likes": {
+					likesEnabled = reader.nextBoolean();
+					return true;
+				}
 				default: {
 					return false;
 				}
@@ -224,6 +230,8 @@ public class DvachModelMapper {
 		String name = null;
 		String tripcode = null;
 		ArrayList<Icon> icons = null;
+		int likes = 0;
+		int dislikes = 0;
 
 		reader.startObject();
 		while (!reader.endStruct()) {
@@ -387,6 +395,14 @@ public class DvachModelMapper {
 					}
 					break;
 				}
+				case "likes": {
+					likes = reader.nextInt();
+					break;
+				}
+				case "dislikes": {
+					dislikes = reader.nextInt();
+					break;
+				}
 				default: {
 					reader.skip();
 					break;
@@ -449,6 +465,10 @@ public class DvachModelMapper {
 		post.setIdentifier(identifier);
 		post.setTripcode(tripcode);
 		post.setCapcode(capcode);
+
+		if (likes != 0 || dislikes != 0) {
+			post.setVote(likes, dislikes);
+		}
 
 		if (userAgentData != null) {
 			int index1 = userAgentData.indexOf('(');
