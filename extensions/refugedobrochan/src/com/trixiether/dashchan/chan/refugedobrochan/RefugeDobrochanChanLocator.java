e5935f81a9ea3f022a04fd2f4caed73c522eed0a
Trixie Ether <trixiether@gmail.com>
2023-05-16 14:39:25 +0300

    TrixiEther | Refuge Dobrochan Links Broken
    Fixed

--- extensions/refugedobrochan/src/com/trixiether/dashchan/chan/refugedobrochan/RefugeDobrochanChanLocator.java
+++ extensions/refugedobrochan/src/com/trixiether/dashchan/chan/refugedobrochan/RefugeDobrochanChanLocator.java
@@ -2,14 +2,28 @@ package com.trixiether.dashchan.chan.refugedobrochan;
 
 import android.net.Uri;
 
+import java.util.regex.Pattern;
+
 import chan.content.VichanChanLocator;
 
 public class RefugeDobrochanChanLocator extends VichanChanLocator {
+
+    private static final Pattern THREAD_PATH = Pattern.compile("/vichan/\\w+/{1,2}res/(\\d+).*\\.html");
 
     public RefugeDobrochanChanLocator() {
         addChanHost("rf.dobrochan.net");
         setHttpsMode(HttpsMode.HTTPS_ONLY);
         DEFAULT_SEGMENT_PRESET = "vichan";
+    }
+
+    @Override
+    public boolean isThreadUri(Uri uri) {
+        return isChanHostOrRelative(uri) && isPathMatches(uri, THREAD_PATH);
+    }
+
+    @Override
+    public String getThreadNumber(Uri uri) {
+        return uri != null ? getGroupValue(uri.getPath(), THREAD_PATH, 1) : null;
     }
 
     @Override
