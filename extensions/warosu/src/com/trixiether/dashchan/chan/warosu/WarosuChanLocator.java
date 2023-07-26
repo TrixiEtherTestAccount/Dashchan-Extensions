780e409eec83bbfa0c7e546cf5eca87d0e48ce95
TrixiEther <trixiether@gmail.com>
2023-07-26 18:27:49 +0300

    TrixiEther | Warosu support
    Warosu module 1.0 release

--- extensions/warosu/src/com/trixiether/dashchan/chan/warosu/WarosuChanLocator.java
+++ extensions/warosu/src/com/trixiether/dashchan/chan/warosu/WarosuChanLocator.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import chan.content.FoolFuukaChanLocator;
 
 public class WarosuChanLocator extends FoolFuukaChanLocator {
+
+    private static int SEARCH_STEP = 24;
+
     public WarosuChanLocator() {
         addChanHost("warosu.org");
         setHttpsMode(HttpsMode.HTTPS_ONLY);
@@ -12,6 +15,17 @@ public class WarosuChanLocator extends FoolFuukaChanLocator {
 
     @Override
     public Uri createBoardUri(String boardName, int pageNumber) {
-        return pageNumber > 0 ? buildPath(boardName, "page", "?task=page&page=" + pageNumber) : buildPath(boardName, "");
+        return pageNumber > 0 ? buildPath(boardName, "?task=page&page=" + pageNumber) : buildPath(boardName, "");
     }
+
+    @Override
+    public Uri createThreadUri(String boardName, String threadNumber) {
+        return buildPath(boardName, "thread", threadNumber);
+    }
+
+    public Uri createSearchUri(String boardName, String text, int pageNumber) {
+        String offset = "&offset=" + (pageNumber * SEARCH_STEP);
+        return buildPath(boardName, "?task=search&ghost=false&search_text=" + text + offset);
+    }
+
 }
